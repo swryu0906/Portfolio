54be80fd7d035a9f022db6ffc2a33cc08b881221
Sungwon Ryu <swryu0906@gmail.com>
2015-08-31 11:50:42 -0400

    Build image scaffold

---
+++ app/views/images/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @image, :id, :name, :description, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831132801) do
+ActiveRecord::Schema.define(version: 20150831153147) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "projects", force: :cascade do |t|
